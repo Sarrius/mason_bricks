1a96ca0750ab55f66c7a6c69c601c9f9904bcf95
vitalii <vitalii.shymko@youraxles.com>
2023-05-04 14:47:14 +0300

    feature_brick

---
+++ bricks/clean_feature/__brick__/{{feature_name}}/{{#is_freezed}}manager{{/is_freezed}}/manager/{{feature_name}}_bloc.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part '{{feature_name}}_bloc.freezed.dart';
+
+part '{{feature_name}}_event.dart';
+
+part '{{feature_name}}_state.dart';
+
+class {{feature_name.pascalCase()}}Bloc extends Bloc<{{feature_name.pascalCase()}}Event, {{feature_name.pascalCase()}}State> {
+  {{feature_name.pascalCase()}}Bloc() : super(const {{feature_name.pascalCase()}}State.initial()) {
+    on<_Started>(_mapStartedToState);
+  }
+}
---
+++ bricks/clean_feature/__brick__/{{feature_name}}/{{feature_name}}_module.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class {{feature_name.pascalCase()}}Module extends Module {
+@override
+List<Module> get imports => [];
+
+@override
+List<Bind> get binds => [];
+
+@override
+List<ModularRoute> get routes => [];
+}
